c8bb187dee935233343b135722b8023997101262
imcamiloup <luiscgr97@gmail.com>
2023-04-19 20:48:04 -0500

    homework CC

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,23 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var decimal=0;
+   for (let i=0; i<num.length;i++){
+      decimal=decimal+num[i]*2**(num.length-i-1);
+   }
+   return decimal;
+   
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   
+   var binario='';
+   while(num!==0){
+      binario= (num%2)+binario;
+      num=Math.floor(num/2);
+   }
+   return binario;
+}
 
 module.exports = {
    BinarioADecimal,
